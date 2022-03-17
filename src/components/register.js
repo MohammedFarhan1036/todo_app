cc72aaf2e100306f84e024d2e6d688247a5ac739
MohammedFarhan1036 <mdfarhan8970344897@gmail.com>
2022-03-16 23:44:03 -0700

    h

---
+++ src/components/register.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+class Register extends Component {
+    state = {  } 
+    render()
+    { 
+        return (
+        <div className="form">
+             <div  class="textfield"> 
+            <p>ENTER FULL NAME: </p>
+            <input type="text "  class="input-group-text input1" name="full_name" placeholder="ENTER FULL NAME"/>
+            </div>
+         <div  class="textfield"> 
+            <p>ENTER FULL NAME: </p>
+            <input type="text "  class="input-group-text input1" name="full_name" placeholder="ENTER FULL NAME"/>
+        </div>
+        
+        <div  class="textfield"> 
+            <p>ENTER FULL NAME: </p>
+            <input type="text "  class="input-group-text input1" name="full_name" placeholder="ENTER FULL NAME"/>
+        </div>
+        <button type="button" value="submit" class="btn btn-success m">SUBMIT</button>
+
+
+        </div>
+        );
+    }
+}
+ 
+export default Register;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import Inputsd from './components/input';
-import Tasks from "./components/tasks.js"
+import Tasks from "./components/tasks.js";
+import Register from "./components/register.js"
 
 class Main extends Component {
-    state = { name:"Arena" } 
+    state = { name:"Arena",
+                count:2        
+} 
+renderthis=()=>{
+    switch(this.state.count){
+        case 1:return(<React.Fragment>
+            <Inputsd state={this.state}/>
+            <div className="tasks">
+            <Tasks/>
+            </div>
+            </React.Fragment>
+         )
+         case 2:return <Register/>
+    }
+
+}
     render() { 
         return (
-            <React.Fragment>
-                <Inputsd state={this.state}/>
-                <div className="tasks">
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                <Tasks/>
-                </div>
-            </React.Fragment>
+            this.renderthis()
 
         );
     }
